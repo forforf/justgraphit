0a245b6dffc05ce58fb6acbd62d675c6a0444df8
David M <dave@radiusnetworks.com>
2016-01-07 15:46:38 -0500

    update faq with angular2 note

--- app/services/faq/faq.ts
+++ app/services/faq/faq.ts
@@ -4,5 +4,6 @@ qas[0] = ["Where is my data stored?", "The data is stored on the local device, t
 qas[1] = ["Do I need a network connection?", "Only to get to the initial page, after that no network is required."];
 qas[2] = ["How much is it?", "Free."];
 qas[3] = ["I don\'t see any ads?", "No Ads."];
+qas[4] = ["How was this built?", "Originally on Angular1, but it has been rewritten in Angular2"];
 
 export var QAs: Array< string[] > = qas;
