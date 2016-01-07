c8a73a2471cccff6380284ce38b1b39113cb2a40
David M <dave@radiusnetworks.com>
2016-01-07 15:49:31 -0500

    update faq with instructions

--- app/services/faq/faq.ts
+++ app/services/faq/faq.ts
@@ -5,5 +5,6 @@ qas[1] = ["Do I need a network connection?", "Only to get to the initial page, a
 qas[2] = ["How much is it?", "Free."];
 qas[3] = ["I don\'t see any ads?", "No Ads."];
 qas[4] = ["How was this built?", "Originally on Angular1, but it has been rewritten in Angular2"];
+qas[5] = ["How do I use it?", "Enter a number you want plotted on the graph page. That's it, everything else is optional."];
 
 export var QAs: Array< string[] > = qas;
