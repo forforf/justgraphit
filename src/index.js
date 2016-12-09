572003ae5f3d3577e2650ab6666575325f0df2c8
David M <dmarti21@gmail.com>
2016-12-09 14:46:58 -0500

    Add about page

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import App from './App';
 import './index.css';
 import { Router, Route, hashHistory } from 'react-router';
 
+// Pages
+import About from './pages/About';
+
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
   </Router>
   ), document.getElementById('root')
 );
