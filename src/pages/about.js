5cfc8ed456418cd7a4f013210814abd9a6cc2389
David M <dmarti21@gmail.com>
2016-12-09 14:54:35 -0500

    Add roboto font

---
+++ src/pages/about.js
@@ -0,0 +1,37 @@
+
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return (
+        <div class="container">
+          <div class="row">
+
+            <div class="col-md-6 graph-data-list">
+              <h3>JustGraphIt</h3>
+              <ul class="list-group"><h4>FAQ (was angularized)</h4>
+                <li>
+                <h5>Q</h5>
+                A
+              </li>
+
+
+            </ul>
+            <ul class="list-group">
+              <li class="list-group-item">
+                version: 0.2.0
+              </li>
+              <li class="list-group-item">
+                author email: <a href="mailto: dmarti21@gmail.com">forforf c/o Dave Martin</a>
+              </li>
+              <li class="list-group-item">
+                license: MIT
+              </li>
+
+            </ul>
+          </div>
+        </div>
+       </div>
+    )
+  }
+})
