a1e93578fc902ed5b7e4f6bc2a672efe0159b3d2
David M <dmarti21@gmail.com>
2016-12-11 13:29:18 -0500

    Rename GraphChart to GraphChooser

--- src/app/pages/Graphit/Graphit.js
+++ src/app/pages/Graphit/Graphit.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import './Graphit.css';
 import GraphInput from 'components/GraphInput/GraphInput'
-import GraphChart from 'components/GraphData/GraphData'
+import GraphChart from 'components/GraphChooser/GraphChooser'
 
 
 class Graphit extends Component {
