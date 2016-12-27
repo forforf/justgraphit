1b1f7ab6a68dd95706992e341692e6c9403663e0
David M <dmarti21@gmail.com>
2016-12-26 22:40:22 -0500

    Renamed component
    GraphChooser is actually a subcomponent of the overall graph viewer. So create component to hold all of the graph views (the graph and the graph chooser). So we can make a specific component called "GraphChooser"

--- src/app/pages/Graphit/Graphit.js
+++ src/app/pages/Graphit/Graphit.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import './Graphit.css';
 import GraphInput from 'components/GraphInput/GraphInput'
-import GraphChart from 'components/GraphChooser/GraphChooser'
+import GraphChart from 'components/GraphViewer/GraphViewer.js'
 
 
 class Graphit extends Component {
