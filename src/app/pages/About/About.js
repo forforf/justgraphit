33ab905a364866e709d220838646cc10ccc6b094
David M <dmarti21@gmail.com>
2017-01-08 16:17:10 -0500

    Fixup FAQ

--- src/app/pages/About/About.js
+++ src/app/pages/About/About.js
@@ -1,11 +1,12 @@
 
 import React from 'react'
 import Faq from 'Faq/Faq';
+import './About.css';
 
 export default React.createClass({
   render() {
     return (
-      <div>
+      <div className="About">
         <h3>JustGraphIt</h3>
         <h4>FAQ</h4>
         <Faq />
@@ -15,7 +16,7 @@ export default React.createClass({
             version: 0.3.0
           </li>
           <li>
-            author email: <a href="mailto: dmarti21@gmail.com">forforf c/o Dave Martin</a>
+            author email: <a href="mailto: dmarti21@gmail.com">dmarti21@gmail.com</a>
           </li>
           <li>
             license: MIT
