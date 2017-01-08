ec834f7d54dcf1ce460d79d11897b683f9f427fc
David M <dmarti21@gmail.com>
2017-01-08 14:19:36 -0500

    Clean up

--- src/app/pages/Editor/Editor.js
+++ src/app/pages/Editor/Editor.js
@@ -3,11 +3,7 @@ import React, { Component } from 'react';
 import Edit from 'EditorView/Edit';
 
 class Editor extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
+  
   render() {
     return (
         <Edit storage={this.props.storage}/>
