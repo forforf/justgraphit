0760ab052ed46ee2dafdea80091583800e636942
forforf <dmarti21@gmail.com>
2022-10-10 11:29:10 -0400

    Handle possible null in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const container = document.getElementById('root')
-const root = createRoot(container!)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const mountPoint = 'root'
+const container = document.getElementById(mountPoint)
+
+if (container !== null) {
+    const root = createRoot(container)
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>,
+    );
+} else {
+  console.warn(`React could not find mount point with element id of: ${mountPoint}`)
+}
+
+
+
